af4211a6280edbd9a1620212023d422ddea4c866
emptizen <emptizen.li@gmail.com>
2024-09-14 22:43:09 +0800

    Sync dotfiles 2024-09-14 22:43:09

--- sync.sh
+++ sync.sh
@@ -17,12 +17,10 @@ sync_file() {
     fi
 }
 
-sync_file "$HOME/.gitconfig" "git/.gitconfig" 
+sync_file "$HOME/.gitconfig" "git/.gitconfig"
 sync_file "$HOME/.vimrc" "vim/.vimrc"
 sync_file "$HOME/.zshrc" "zsh/.zshrc"
 
 echo "Dotfiles have been synced from your home directory to the "$PWD" directory."
 
-git add . 
-git commit -m "Sync dotfiles $(date "+%Y-%m-%d %H:%M:%S")"
-git push
+git add . &&  git commit -m "Sync dotfiles $(date "+%Y-%m-%d %H:%M:%S")" && git push
