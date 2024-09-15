fa68d1a96559c228a04b02990ef1573459000233
emptizen <emptizen.li@gmail.com>
2024-09-15 20:45:01 +0800

    Sync dotfiles 2024-09-15 20:45:01

--- init.sh
+++ init.sh
@@ -74,45 +74,12 @@ install_nvm() {
 }
 
 # Call the functions to install each tool
-# Parse command line arguments
-while [[ $# -gt 0 ]]; do
-    case "$1" in
-        --cronjob)
-            add_cronjob
-            shift
-            ;;
-        --homebrew)
-            install_homebrew
-            shift
-            ;;
-        --oh-my-zsh)
-            install_oh_my_zsh
-            shift
-            ;;
-        --vim-plug)
-            install_vim_plug
-            shift
-            ;;
-        --sdkman)
-            install_sdkman
-            shift
-            ;;
-        --nvm)
-            install_nvm
-            shift
-            ;;
-        *)
-            echo "Unknown option: $1"
-            exit 1
-            ;;
-    esac
-done
-
-# If no arguments were provided, print usage
-if [ $# -eq 0 ]; then
-    echo "Usage: $0 [--cronjob] [--homebrew] [--oh-my-zsh] [--vim-plug] [--sdkman] [--nvm]"
-    echo "Use one or more options to install the desired tools."
-fi
+add_cronjob 
+install_homebrew
+install_oh_my_zsh
+install_vim_plug
+install_sdkman
+install_nvm
 
 # Check and copy each dotfile
 check_and_copy "git/.gitconfig" "$HOME/.gitconfig"
